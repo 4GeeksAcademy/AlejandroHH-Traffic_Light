a73c2a521955d9e9b1886ec810aec73cddbd8580
Alejandro Hipólito Hernando <126016967+Alejandro-Hipolito@users.noreply.github.com>
2023-05-08 22:29:49 +0000

    Creation of a basic traffic light.

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
+
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
+
+
 //create your first component
 const Home = () => {
+
+	const [pick, setPick] = useState("")
+
+	const redClass = pick === "red" ? "redGlow" : "red";
+	const yellowClass = pick === "yellow" ? "yellowGlow" : "yellow";
+	const greenClass = pick === "green" ? "greenGlow" : "green";
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="d-flex justify-content-center mt-5">
+		<div className="traffic-light">
+			<div onClick={x => setPick("red")} className={`light red ${redClass}`}></div>
+			<div onClick={x => setPick("yellow")} className={`light yellow ${yellowClass}`}></div>
+			<div onClick={x => setPick("green")} className={`light green ${greenClass}`}></div>
 		</div>
+	</div>
 	);
 };
 
